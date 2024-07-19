3dd84fbc21f7eaca2fbc09f377a3bf1eeaab4e68
Vitor Hugo <vitorh565.vh@gmail.com>
2024-07-19 09:42:16 -0300

    .

---
+++ Capitulo2/Ordenação_Por_Seleção.py
@@ -0,0 +1,4 @@
+# Array = significa que todas as suas tarefas estão armazenadas uma ao lado da outra na memoria (Sabemos os endereços do array)
+
+# Lista encadeada = Seus itens podem estar em qualquer lugar da memoria (Pelos elementos não estarem proximos um dos outros não sabemos a posição especifica)
+    # -> Listas encadeadas tem leitura lenta(O(n)) e inserção rapida (O(1))

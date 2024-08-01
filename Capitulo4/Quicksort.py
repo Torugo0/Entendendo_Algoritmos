72acdbdd22d1a6fb8f93acea40294da35eb873ac
Vitor Hugo <vitorh565.vh@gmail.com>
2024-08-01 09:29:58 -0300

    .

--- Capitulo4/Quicksort.py
+++ Capitulo4/Quicksort.py
@@ -37,3 +37,20 @@ def maxima(lista):
     return lista[0] if lista[0] > sub_max else sub_max
 
 print(maxima([2,4,6,10,11]))
+
+# QUICKSORT
+
+# Muito mais rápido que a ordenação por seleção 
+
+# Caso-Base em que um array não precisa ou não pode ser ordenado
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivo = array[0]
+        menores = [i for i in array[1:] if i <= pivo]
+        maiores = [i for i in array[1:] if i > pivo]
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+    
+print(quicksort([10,5,2,3,20]))
